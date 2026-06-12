78b980f88f148ded8db2e8311d31b3bfe3317259
mppon <wangb.edu@gmail.com>
2026-06-12 09:28:25 +0800

    feat: agent

--- src/agent.ts
+++ src/agent.ts
@@ -1,17 +1,23 @@
 /**
  * agent的实现
  * message的维护
+ * tool call
  * agent loop
  */
 
 import type { LLMClient } from './llm'
-import type { Message } from './llm/type'
+import type { Message, Tool } from './llm/type'
 
 export interface AgentConfig {
   llmClient: LLMClient
   tools?: Array<any>
   systemPrompt?: string
   maxSteps?: number
+}
+
+export interface InjectorConfig {
+  onMessage?: (message: Message) => void
+  onToolCall?: (tool: Tool) => void
 }
 
 export class Agent {
@@ -37,17 +43,25 @@ export class Agent {
     this.messages.push(msg)
   }
 
-  public async run() {
+  public get_all_messages() {
+    return this.messages
+  }
+
+  public async run(injector: InjectorConfig) {
     let step = 0
     // agent loop
     while (step < this.maxSteps) {
       const response = await this.llmClient.generate(this.messages, this.tools)
-      this.messages.push({
-        role: 'assistant',
+      const message = {
+        role: 'assistant' as const,
         content: response.content,
         thinking: response.thinking,
         tool_calls: response.tool_calls,
-      })
+      }
+      this.messages.push(message)
+
+      // loop过程存在多个消息，抛出供外部消费
+      injector?.onMessage && injector.onMessage(message)
 
       // 如果没有工具调用，则认为对话结束
       if (!response.tool_calls || response.tool_calls.length === 0) {
@@ -59,6 +73,8 @@ export class Agent {
         for (const toolCall of response.tool_calls) {
           // 这里简单的模拟工具调用，实际可以根据toolCall.name去调用不同的工具
           const tool_response = `工具${toolCall.name}的调用结果`
+          // 供外部消费
+          injector?.onToolCall && injector?.onToolCall(toolCall)
           this.messages.push({
             // tool call的结果role为tool，client以此识别用户输入还是工具调用结果
             role: 'tool',
